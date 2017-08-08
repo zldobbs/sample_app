957a2f1199a75c562b2fb6ef62a77d7f9ee96acd
Zach <dobbszach@gmail.com>
2017-08-08 16:00:35 -0500

    Fix length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates(:name, presence: true, length: {maximum: 50})
-  validates(:email, presence: true, length: {maximum: 256})
+  validates(:email, presence: true, length: {maximum: 255})
 end
