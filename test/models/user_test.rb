d63066a903675a33023c9ece4c9df4348239178b
Zach <dobbszach@gmail.com>
2017-08-07 21:11:49 -0500

    Setup some user validation prereqs

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,21 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "User Name", email: "user@email.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
 end
