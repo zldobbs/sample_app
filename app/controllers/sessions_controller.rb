afc9260433c06127f641040e350370147de57f29
Zach <dobbszach@gmail.com>
2017-09-03 20:49:29 -0500

    Add initial error message for signing incorrectly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #login success
+    else
+      #login failed, display error
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
