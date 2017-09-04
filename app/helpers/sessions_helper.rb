edea41efc3d50ed4b3751c97fc443eb75e78c192
Zach <dobbszach@gmail.com>
2017-09-03 21:23:34 -0500

    Create new view for logged in users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,14 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # Find the current user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Check if a user is currently logged in
+  def logged_in?
+    !current_user.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
