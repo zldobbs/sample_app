c887f48590a353c7e43b5344009d5d3ef9ae9957
Zach <dobbszach@gmail.com>
2017-09-03 21:06:13 -0500

    Add handling for good login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #login success
+      log_in(user)
+      redirect_to user
     else
       #login failed, display error
       flash.now[:danger] = 'Invalid email/password combination'
