defa9aa1b001a8c80aa6362e46ecfe08b20111cb
Zach <dobbszach@gmail.com>
2017-09-03 20:53:46 -0500

    Add integration test for flash on incorrect login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       #login success
     else
       #login failed, display error
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
