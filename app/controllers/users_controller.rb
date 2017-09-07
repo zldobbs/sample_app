5f7cc17c19196fc8242d670f4414830ac115ebe2
Zach <dobbszach@gmail.com>
2017-09-06 19:24:07 -0500

    Change brand to "Titter"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to Titter! Tee Hee!"
       redirect_to @user
     else
       render 'new'
